0519197c62e0ac6c9ec92ae2d902637ada4afab1
BV-14 <bvasquezva@usm.cl>
2024-11-11 23:45:38 -0300

    Update forms.py

--- inclured1/forms.py
+++ inclured1/forms.py
@@ -1,1 +1,9 @@
+# forms.py
+from django import forms
+from .models import Usuario
+
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ['correo', 'nombre', 'cod_disc']
 
