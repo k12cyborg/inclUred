5f7b068c2562552a2e81e32e2431930a3b46a9a8
BV-14 <bvasquezva@usm.cl>
2024-11-14 00:37:45 -0300

    Update admin.py

--- inclured1/admin.py
+++ inclured1/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Usuario, Discapacidad, Anecdota
 
-# Register your models here.
+# Registrar modelos en el panel de administración
+admin.site.register(Usuario)
+admin.site.register(Discapacidad)
+admin.site.register(Anecdota)
